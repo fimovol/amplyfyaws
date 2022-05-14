15a7e960fd7c4c3df69fec46e05ec01cbeb9b1c1
fimovol <fimovolpe@gmail.com>
2022-05-14 10:43:54 -0500

    added auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hola mundo desde react
-        </p>
-      </header>
-    </div>
+    <Authenticator>
+      {({signOut,uer})=>(
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            hola mundo desde react
+          </p>
+          <button onClick={signOut}>Sign out</button>
+        </header>
+
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
